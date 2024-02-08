172933edc2f5fef667ab487683faa33352193a49
abdoo9 <63903814+abdoo9@users.noreply.github.com>
2024-02-08 20:27:57 +0300

    Initialize example/multiple-bots

---
+++ src/server/index.ts
@@ -0,0 +1,37 @@
+import fastify from "fastify";
+import { BotError, webhookCallback } from "grammy";
+import type { Bot } from "#root/bot/index.js";
+import { errorHandler } from "#root/bot/handlers/index.js";
+import { logger } from "#root/logger.js";
+
+export const createServer = async ({
+  getBot,
+}: {
+  getBot: (token: string) => Promise<Bot>;
+}) => {
+  const server = fastify({
+    logger,
+  });
+
+  server.setErrorHandler(async (error, request, response) => {
+    if (error instanceof BotError) {
+      errorHandler(error);
+
+      await response.code(200).send({});
+    } else {
+      logger.error(error);
+
+      await response.status(500).send({ error: "Oops! Something went wrong." });
+    }
+  });
+
+  server.get("/", () => ({ status: true }));
+
+  server.post("/:token([0-9]+:[a-zA-Z0-9_-]+)", async (request, response) => {
+    const { token } = request.params as { token: string };
+
+    return webhookCallback(await getBot(token), "fastify")(request, response);
+  });
+
+  return server;
+};
